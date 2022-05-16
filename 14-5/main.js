3ad482271dc80258bd1f43a64737c52b109e112b
mahesh <maheshmb7712@gmail.com>
2022-05-16 08:15:46 +0530

    first commit

---
+++ 14-5/main.js
@@ -0,0 +1,3 @@
+console.log(firstname,secondName,age,city,country,isMarried);
+console.log(gravity,boilingpoint,PI);
+console.log(name,age,live);
---
+++ 14-5/variable.js
@@ -0,0 +1,19 @@
+
+// declaring diffrenet type of variable
+
+let firstname='mahesh'
+let secondName='mb'
+let age =22
+let city ='enathu'
+let country ='india'
+let isMarried=true
+
+// declaring variables with number values
+
+const gravity=9.8
+const PI=3.14
+const boilingpoint =100
+
+// variables can also declaring by separating them with comma
+
+let name='mahesh',job='software engineer',live='india'
