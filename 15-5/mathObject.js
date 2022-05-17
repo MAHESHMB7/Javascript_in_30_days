cc6ecae9070b0b0aaedd8153fcfec94d8fb7130b
mahesh <maheshmb7712@gmail.com>
2022-05-17 21:44:20 +0530

    commit

---
+++ 15-5/mathObject.js
@@ -0,0 +1,10 @@
+const PI=Math.PI
+console.log(PI);
+
+console.log(Math.round(PI));
+console.log(Math.random());
+
+
+console.log(Math.random()* 11);
+
+
---
+++ 15-5/primitiveDatatypes.js
@@ -0,0 +1,12 @@
+let nums =[10,11,12]
+console.log(nums[0]);
+console.log(nums);
+
+
+let object={
+    name:'mahesh',
+    job:'isro trainee',
+}
+
+
+
